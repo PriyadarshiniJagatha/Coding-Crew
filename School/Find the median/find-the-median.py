308a1e7a21edddfc4c4d2454448856821c31accb
Priyadarshini Jagatha <144915248+PriyadarshiniJagatha@users.noreply.github.com>
2024-05-16 23:44:29 +0530

    Added Solution - GfG to GitHub

---
+++ School/Find the median/find-the-median.py
@@ -0,0 +1,26 @@
+#User function Template for python3
+
+class Solution:
+	def find_median(self, v):
+		v.sort()
+	    n=len(v)
+	    if n%2==0:
+	        med=(v[n//2]+v[(n//2)-1])//2
+	    else:
+	        med=v[(n//2)]
+	    return med
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+	T=int(input())
+	for i in range(T):
+		n = int(input())
+		v = list(map(int,input().split())) 
+		ob = Solution();
+		ans = ob.find_median(v)
+		print(ans)
+# } Driver Code Ends
